6cdc9bafc9728d8a625797ee40e38174f2c2dcca
alamin.147 <alamin.14780@gmail.com>
2024-11-09 14:07:09 +0600

    first commit

---
+++ snippet.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define py cout << "YES" << "\n"
+#define pn cout << "NO" << "\n"
+#define forn for (int i = 0; i < n; i++)
+#define fornj for (int j = 0; j < n; j++)
+#define ll long long int
+#define elif else if
+#define nl "\n"
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+ 
+   int t;
+   cin >> t;
+   while (t--)
+   {
+      
+   }
+}
+
+
+
+
+echo "# code-snippet" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:alamin147/code-snippet.git
+git push -u origin main
